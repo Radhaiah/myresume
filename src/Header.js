afac553b48137003bb5668a173f7747765e843d6
Radhaiah <94778126+Radhaiah@users.noreply.github.com>
2022-08-14 20:56:21 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ export default function Header(){
         <div className='header bg-primary text-white'>
             <Button onClick={()=>{navigate('/Carrer')}}>Carrer Object</Button>
             <Button onClick={()=>{navigate('/Technical')}}>Technical Skills</Button>
-            <Button onClick={()=>{navigate('/Experience')}}>Projrcts</Button>
+            <Button onClick={()=>{navigate('/Experience')}}>Projects</Button>
             <Button onClick={()=>{navigate('/Education')}}>Education Details</Button>
             <Button onClick={()=>{navigate('/About')}}>About Me</Button>
         </div>
